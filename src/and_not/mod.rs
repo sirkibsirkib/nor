e74b973e3f42fb86a80955aabff2d82b8c6b0703
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2022-07-28 18:41:27 +0200

    more

--- src/and_not/mod.rs
+++ src/and_not/mod.rs
@@ -6,14 +6,21 @@ use crate::var_set::VarSet;
 use formula::Formula;
 
 pub fn main() {
-    let kb = Kb {
+    let _kb = Kb {
         vars_true: VarSet::from_iter([V[0]]), // true
         vars_fals: VarSet::from_iter([V[0]]), // false
     };
-    let mut formula = Formula::var(V[0]).and(Formula::var(V[1])).and(Formula::var(V[1]));
+    // let mut formula = Formula::var(V[0]).and(Formula::var(V[0]).yimpl(Formula::var(V[1])));
+
+    // A & !(!(A & !B) & !B)
+
+    let mut formula = Formula::var(V[0])
+        .yimpl(Formula::var(V[1]))
+        .and(Formula::var(V[0]))
+        .and(Formula::var(V[1]).not());
     println!("{:#?}\n ==>", formula);
-    formula = formula.var_elim_with(&kb);
-    println!("{:#?}\n ==>", formula);
+    // formula = formula.var_elim_with(&kb);
+    // println!("{:#?}\n ==>", formula);
     formula = formula.simplify();
     println!("{:#?}\n", formula);
 }
